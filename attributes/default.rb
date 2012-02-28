504dcda562046dc22c4deef86fc3aa9cda45faab
Trotter Cashion <cashion@gmail.com>
2012-02-27 21:33:30 -0800

    Initial Commit

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default[:cloudfoundry_common][:ruby_1_9_2_version] = "1.9.2-p290"
+default[:cloudfoundry_common][:ruby_1_9_2_path] = "/opt/ruby-1.9.2-p290"
+
+default[:cloudfoundry_common][:user] = ENV['USER']
---
+++ recipes/ruby_1_9_2.rb
@@ -0,0 +1,4 @@
+include_recipe "rbenv"
+
+rbenv_ruby node.cloudfoundry_common.ruby_1_9_2_version
+
