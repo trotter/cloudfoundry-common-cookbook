722632d54d82db2eb15b902674e439709adf0283
Trotter Cashion <cashion@gmail.com>
2012-03-06 16:27:52 -0800

    Recursively add directories

--- recipes/directories.rb
+++ recipes/directories.rb
@@ -1,14 +1,47 @@
 directory node[:cloudfoundry_common][:config_dir] do
+  recursive true
   owner node[:cloudfoundry_common][:user]
   mode  '0755'
 end
 
 directory node[:cloudfoundry_common][:log_dir] do
+  recursive true
   owner node[:cloudfoundry_common][:user]
   mode  '0755'
 end
 
 directory node[:cloudfoundry_common][:pid_dir] do
+  recursive true
+  owner node[:cloudfoundry_common][:user]
+  mode  '0755'
+end
+
+directory node[:cloudfoundry_common][:staging_cache_dir] do
+  recursive true
+  owner node[:cloudfoundry_common][:user]
+  mode  '0755'
+end
+
+directory node[:cloudfoundry_common][:staging_manifests_dir] do
+  recursive true
+  owner node[:cloudfoundry_common][:user]
+  mode  '0755'
+end
+
+directory node[:cloudfoundry_common][:tmpdir] do
+  recursive true
+  owner node[:cloudfoundry_common][:user]
+  mode  '0755'
+end
+
+directory node[:cloudfoundry_common][:droplets_dir] do
+  recursive true
+  owner node[:cloudfoundry_common][:user]
+  mode  '0755'
+end
+
+directory node[:cloudfoundry_common][:resources_dir] do
+  recursive true
   owner node[:cloudfoundry_common][:user]
   mode  '0755'
 end
