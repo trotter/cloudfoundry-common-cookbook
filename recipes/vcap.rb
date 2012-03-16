a0e8ed0b826a09e8677f172d403e159f310064e6
Trotter Cashion <cashion@gmail.com>
2012-03-16 16:28:56 -0700

    Pull down submodules

--- recipes/vcap.rb
+++ recipes/vcap.rb
@@ -1,6 +1,8 @@
 git node['cloudfoundry_common']['vcap']['install_path'] do
-  repository node['cloudfoundry_common']['vcap']['repo']
-  reference  node['cloudfoundry_common']['vcap']['reference']
-  user       node['cloudfoundry_common']['user']
+  repository        node['cloudfoundry_common']['vcap']['repo']
+  reference         node['cloudfoundry_common']['vcap']['reference']
+  user              node['cloudfoundry_common']['user']
+  depth             1 # Only fetch the latest revision
+  enable_submodules true
   action :sync
 end
