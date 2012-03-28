156afe2273eb5ccaa63a9e4e6dba42c43a5861a0
Trotter Cashion <cashion@gmail.com>
2012-03-28 00:23:37 -0700

    Pull down the entire vcap repo.
    Though this is much slower, it ensures that we're able to checkout the
    revision specified by `reference`. Previous code only worked because our
    revision was the same as the latest revision.

--- recipes/vcap.rb
+++ recipes/vcap.rb
@@ -2,7 +2,6 @@ git node['cloudfoundry_common']['vcap']['install_path'] do
   repository        node['cloudfoundry_common']['vcap']['repo']
   reference         node['cloudfoundry_common']['vcap']['reference']
   user              node['cloudfoundry_common']['user']
-  depth             1 # Only fetch the latest revision
   enable_submodules true
   action :sync
 end
